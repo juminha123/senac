a6dc1c79deab1aa83faf520173e4f6daed15fda5
juminhadgrau <gjuliasouzagomes@gmail.com>
2025-07-10 10:22:12 -0300

    Add files via upload

---
+++ aaa.py
@@ -0,0 +1,11 @@
+print('=== Soma ===')
+a = int(input('Digite um número: '))
+b = int(input('Digite outro número: '))
+soma = a + b 
+print('A soma é ' + str(soma))
+subtração = a - b 
+print('A subtração é ' + str(subtração))
+divisão = a / b 
+print('A divisão é ' + str(divisão))
+multiplicação = a * b 
+print('A multiplicação é ' + str(multiplicação))
