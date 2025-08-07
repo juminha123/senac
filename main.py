f7bd31209204ce7477ac6014332d4d3805234ba3
juminhadgrau <gjuliasouzagomes@gmail.com>
2025-08-07 08:19:16 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+a = int(input("digite um número: "))
+b = int(input("digite outro número: "))
+if(a == b):
+    print("números iguais!")
+else:
+    print("números diferentes")
